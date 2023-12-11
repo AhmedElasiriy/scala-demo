e73f0027c85693697f332aaa201ba282021e2be0
Ahmed Mostafa <ahmed.elasiri1@gmail.com>
2023-12-11 02:14:47 +0200

    Refactor TaskList controller and add login functionality

--- app/controllers/TaskList.scala
+++ app/controllers/TaskList.scala
@@ -7,7 +7,16 @@ import play.api.mvc._
 import play.api.i18n._
 
 @Singleton
-class TaskList @Inject() (cc: ControllerComponents) extends AbstractController(cc)  {
+class TaskListC @Inject() (cc: ControllerComponents) extends AbstractController(cc)  {
+
+    def login = Action {
+        Ok(views.html.login())
+    }
+
+    def validateLogin(username: String, password: String) = Action {
+        Ok(s"username: $username, password: $password")
+    }
+    
 def tasklist = Action {
     val tasks = List("go to the store", "clean the kitchen", "learn some scala")
 Ok(views.html.tasklist(tasks))  
